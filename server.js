5c65c6d71c86c150c4a3f2c81be19db24871230c
Naidu <eshwar.naidu@accenture.com>
2018-04-17 16:18:12 +0530

    some changes

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public')); 
+
+
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var long = `${now}: ${req.method} ${req.url}`
+
+    fs.appendFileSync('server.log', long + '\n');
+    next();
+})
+
+hbs.registerHelper('getYear', () => {
+    return new Date().getFullYear();
+});
+
+app.use((req, res, next) => {
+    res.render('maintanence.hbs', {
+        msg: 'The Site is under maintanence bhai.'
+    });
+});
+
+
 
 app.get('/', (req, res) => {
 //     res.send({
