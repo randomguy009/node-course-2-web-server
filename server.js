6e470875e2352174f71d883ae00d2fd488333dfa
Naidu <eshwar.naidu@accenture.com>
2018-04-18 15:37:18 +0530

    some changes

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 3000;
 
 var app = express();
 
+
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public')); 
@@ -39,7 +40,7 @@ app.get('/', (req, res) => {
 // })
 
         res.render('home.hbs', {
-            pageTitle: 'Home Page hai bhai',
+            pageTitle: 'Home Page hai gandu',
             currentYear: new Date().getFullYear(),
             welcomeMsg: 'Welcome Bruh!!'
         });
